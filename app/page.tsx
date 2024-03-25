8456ada291b7a5dd148c1af670182044dc5fffc5
seano424 <soreilly424@gmail.com>
2024-03-25 16:08:30 +0700

    about us

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { loadingAtom } from '@/atoms'
 import { useAtomValue } from 'jotai'
 import Header from '@/components/Header'
 import NumbersAndFactsGrid from '@/components/NumbersAndFactsGrid'
+import AboutUs from '@/components/AboutUs'
 
 export default function Home() {
   const loading = useAtomValue(loadingAtom)
@@ -19,6 +20,7 @@ export default function Home() {
           <Header />
           <Hero />
           <NumbersAndFactsGrid />
+          <AboutUs />
         </>
       )}
     </main>
---
+++ components/AboutUs.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import clsx from 'clsx'
+
+export default function AboutUs() {
+  return (
+    <div className="grid grid-cols-2 px-4 py-20">
+      <div className="col-start-2 flex flex-col gap-2">
+        <div className="flex justify-between items-center text-xl">
+          <p>About us</p>
+          <p>&copy; 12-24</p>
+        </div>
+
+        <Link
+          href="/"
+          className="flex justify-between items-center bg-black w-full rounded-lg py-3 px-4 text-white relative group  overflow-hidden"
+        >
+          <div className="w-full h-full">
+            <span
+              className={clsx(
+                'absolute py-3 px-4 inset-0 transform transition-transform duration-300 ease-linear translate-y-0',
+                'group-hover:-translate-y-12'
+              )}
+            >
+              Read more
+            </span>
+            <span
+              className={clsx(
+                'absolute py-3 px-4 inset-0 transform transition-transform duration-300 ease-linear translate-y-12',
+                'group-hover:translate-y-0 z-10',
+                'group-hover:bg-gray-700/70'
+              )}
+            >
+              Read more
+            </span>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 relative z-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  )
+}
