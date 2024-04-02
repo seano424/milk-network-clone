68de5c816e3a9953143612b692216192b11e35e2
seano424 <soreilly424@gmail.com>
2024-04-02 21:14:47 +0700

    small update to cursor

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -72,7 +72,7 @@ export default function Cursor() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className="w-2 h-2"
           >
             <path
               strokeLinecap="round"
