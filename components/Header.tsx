dc0837ba787394299e0ed89dc5ae07e66e6f3e93
seano424 <soreilly424@gmail.com>
2024-04-07 13:19:22 +0700

    navbar 2 only show on homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -123,7 +123,10 @@ export default function Header() {
         initial="closed"
         animate={isTop ? 'closed' : 'open'}
         transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-        className="fixed left-0 right-0 top-0 p-4 flex items-center justify-between text-xl bg-white z-30"
+        className={clsx(
+          'fixed left-0 right-0 top-0 p-4 flex items-center justify-between text-xl bg-white z-30',
+          pathname !== '/' && 'hidden'
+        )}
       >
         <Link
           href={'/'}
