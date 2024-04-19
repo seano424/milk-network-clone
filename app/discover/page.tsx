5aa25aae36efed6e03441054ee29e577527cdd79
seano424 <soreilly424@gmail.com>
2024-04-19 11:40:38 +0800

    removing animate presence for page change

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,32 +1,14 @@
-'use client'
 import HeroText from '@/components/HeroText'
-import { AnimatePresence, motion } from 'framer-motion'
 
 export default function DiscoverPage() {
   return (
-    <AnimatePresence
-      initial={false}
-      key="work-page"
-    >
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { duration: 5, ease: 'easeInOut' },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { duration: 5, ease: 'easeInOut' },
-        }}
-        className="mt-20 flex flex-col gap-8"
-      >
-        <HeroText
-          header="The Latest"
-          subheader="Discover our thought leadership and expert insights on vision,
+    <div className="mt-20 flex flex-col gap-8">
+      <HeroText
+        header="The Latest"
+        subheader="Discover our thought leadership and expert insights on vision,
           strategy, culture, and brand, and learn how to apply them to your
           business."
-        />
-      </motion.div>
-    </AnimatePresence>
+      />
+    </div>
   )
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,41 +1,23 @@
-'use client'
-import { AnimatePresence, motion } from 'framer-motion'
 import GridList from '@/components/GridList'
 import CtaLarge from '@/components/CtaLarge'
 import HeroText from '@/components/HeroText'
 
 export default function WorkPage() {
   return (
-    <AnimatePresence
-      initial={false}
-      key="work-page"
-    >
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { duration: 5, ease: 'easeInOut' },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { duration: 5, ease: 'easeInOut' },
-        }}
-        className="mt-20 flex flex-col gap-8"
-      >
-        <HeroText header="Work" />
-        <div className="grid gap-40 p-4">
-          <GridList />
-          <CtaLarge
-            image="https://strapi-cms-3mz0.onrender.com/uploads/Jeddah_Office_1_70052261e5.webp"
-            fullWidth={false}
-            dot={false}
-            heading="Let's work"
-            headingLine2=""
-            headingLine3=""
-            rightHeading="Be the change you want to see - act now!"
-          />
-        </div>
-      </motion.div>
-    </AnimatePresence>
+    <div className="mt-20 flex flex-col gap-8">
+      <HeroText header="Work" />
+      <div className="grid gap-40 p-4">
+        <GridList />
+        <CtaLarge
+          image="https://strapi-cms-3mz0.onrender.com/uploads/Jeddah_Office_1_70052261e5.webp"
+          fullWidth={false}
+          dot={false}
+          heading="Let's work"
+          headingLine2=""
+          headingLine3=""
+          rightHeading="Be the change you want to see - act now!"
+        />
+      </div>
+    </div>
   )
 }
